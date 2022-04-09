052e7dc0006a1382ce626530a521087bb5fa499e
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-04-08 22:29:59 -0700

    fixed for loop

--- HW3A/ll1.cpp
+++ HW3A/ll1.cpp
@@ -163,11 +163,11 @@ int main() {
     int i = 0;               // index for ss
     while (ss[i] != '\0') {  // for each char of ss
         string remainingInput = "";
-        for (string::size_type j = 0; j < ss.length(); j++) {
+        for (int j = i; j < ss.length(); j++) {
             remainingInput += ss[j];
         }
         cout << "remaining input string: " << remainingInput << endl;
-        
+
         displayStack();
         if (parseStack.empty()) {
             cout << "mismatch: stack is empty" << endl;
